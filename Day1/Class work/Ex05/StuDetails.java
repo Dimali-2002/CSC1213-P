f768ae9da505c49b54ef5ccfd26c1ea302f793a9
Dimali-2002 <lasanthikadimali@gmail.com>
2025-03-15 19:31:46 -0700

    Upload Ex05

---
+++ Day1/Class work/Ex05/StuDetails.java
@@ -0,0 +1,17 @@
+class StuDetails{
+	public static void main(String args[])
+	{
+		System.out.println("Name: Sountharajah Janany");
+		System.out.println("Registration Number:2018/ASP/01");
+		System.out.println("Degree Programme:BSc.ComputerScience");
+		System.out.println();
+		
+		String Name="Sountharajah Janany";
+		String RegNo="2018/ASP/01";
+		String Degree="BSc.ComputerScience";
+		
+		System.out.println("Full Name : "+Name);
+		System.out.println("Registration Number : "+RegNo);
+		System.out.println("Degree Programme : "+Degree);
+	}
+}
