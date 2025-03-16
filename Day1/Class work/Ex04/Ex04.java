1a577bf686f1e408f4be77ec4ea19b4c57ee8927
Dimali-2002 <lasanthikadimali@gmail.com>
2025-03-15 19:22:34 -0700

    Upload Ex04

---
+++ Day1/Class work/Ex04/Ex04.java
@@ -0,0 +1,14 @@
+class Ex04{
+	public static void main(String[] args)
+	{
+		int x=10;
+		int y=2;
+		
+		System.out.println("Addition is : "+(x+y));
+		System.out.println("Substraction is : "+(x-y));
+		System.out.println("Multiplication is : "+(x*y));
+		System.out.println("Division is : "+(x/y));
+		System.out.println("Modulus is : "+(x%y));
+		
+	}
+}
