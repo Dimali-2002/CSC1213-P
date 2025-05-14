89e4d0326df81da01c8e98ffa47627349b473e53
Dimali-2002 <lasanthikadimali@gmail.com>
2025-05-14 09:57:48 -0700

    Upload Ex01

---
+++ Day8/Class work/Ex01/ex1.java
@@ -0,0 +1,11 @@
+public class ex1{
+	int x = 5;
+	
+	public static void main(String args[])
+	{
+		ex1 value1 = new ex1();
+		ex1 value2 = new ex1();
+		System.out.println(value1.x);
+		System.out.println(value2.x);
+	}	
+}
