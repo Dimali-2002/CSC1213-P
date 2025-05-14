95eb30efac6bc75f846a5b9cbca051dc88796f8e
Dimali-2002 <lasanthikadimali@gmail.com>
2025-05-14 09:48:53 -0700

    Upload Ex07

---
+++ Day7/Class Work/Ex07/Ex2.java
@@ -0,0 +1,12 @@
+public class Ex2{
+	
+	static int cube (int number)
+	{
+		return number*number*number;
+	}
+	public static void main(String args[])
+	{
+		int result = cube(3);
+		System.out.println("The cube is "+result+" of 3");
+	}	
+}
