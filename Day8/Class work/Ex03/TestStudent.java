ef89ade3db6cc3af2c9e62a8b9e3e52f6920295a
Dimali-2002 <lasanthikadimali@gmail.com>
2025-05-14 10:01:16 -0700

    Upload Ex03

---
+++ Day8/Class work/Ex03/TestStudent.java
@@ -0,0 +1,17 @@
+class Student{
+	int id;
+	String name;
+	
+	public Student(int x, String y)
+	{
+		this.id = x;
+		this.name = y;
+	}
+}
+class TestStudent{
+	public static void main(String args[])
+	{
+		Student s1 = new Student(01,"Pavani");
+		System.out.println(s1.id+" "+s1.name);
+	}
+}
