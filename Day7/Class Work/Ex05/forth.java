eaab8ed55066100c6fed9294c5a1473cbcaca9d2
Dimali-2002 <lasanthikadimali@gmail.com>
2025-05-14 09:44:31 -0700

    Upload Ex05

---
+++ Day7/Class Work/Ex05/forth.java
@@ -0,0 +1,18 @@
+public class forth{
+	static int sum(int a, int b)
+	{
+		return a+b;
+	}
+	static double sum(double a, double b)
+	{
+		return a+b;
+	}
+	public static void main(String args[])
+	{
+		int intResult = sum(5, 10);
+		double doubleResult = sum(3.5, 2.5);
+		
+		System.out.println("sum(int) : "+intResult);
+		System.out.println("sum(double) : "+doubleResult);
+	}
+}
