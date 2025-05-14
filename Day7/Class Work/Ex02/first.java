5c37673b360ffdcee5a07162124353d3862fc62a
Dimali-2002 <lasanthikadimali@gmail.com>
2025-05-14 09:39:31 -0700

    Upload Ex02

---
+++ Day7/Class Work/Ex02/first.java
@@ -0,0 +1,11 @@
+public class first{
+	static void printDetails(String name, int age)
+	{
+		System.out.println("Name: "+name);
+		System.out.println("Age: "+age);
+	}
+	public static void main(String args[])
+	{
+		printDetails("Kamal",25);
+	}
+}
