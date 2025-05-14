8c414b20458767aa08dc9239d82470139feb2faa
Dimali-2002 <lasanthikadimali@gmail.com>
2025-05-14 09:55:51 -0700

    Upload Ex11

---
+++ Day7/Class Work/Ex11/Ex6.java
@@ -0,0 +1,11 @@
+public class Ex6{
+	static double average(int a,int b,int c)
+	{
+		return (a+b+c)/3.0;
+	}
+	public static void main(String args[])
+	{
+		double result = average(4, 7, 10);
+		System.out.println("Average: "+result);
+	}
+}
