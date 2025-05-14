6fd66bd530967647f0b0cf7f64c02c30b0d4370e
Dimali-2002 <lasanthikadimali@gmail.com>
2025-05-14 09:59:41 -0700

    Upload Ex02

---
+++ Day8/Class work/Ex02/Student2.java
@@ -0,0 +1,14 @@
+class Student{
+	int id;
+	String name;
+}
+
+class Student2{
+	public static void main(String args[])
+	{
+		Student s1 = new Student();
+		s1.id = 01;
+		s1.name = "Pavani";
+		System.out.println(s1.id+" "+s1.name);
+	}
+}
