cf1fe2d9ed2a27ac4f0368e50689f82aae8a23ec
Dimali-2002 <lasanthikadimali@gmail.com>
2026-02-02 14:04:19 +0530

    Add files via upload

---
+++ Day10/Ex02/Main2.java
@@ -0,0 +1,24 @@
+abstract class Animal
+{
+	public abstract void animalSound();
+	public void sleep()
+	{
+		System.out.println("zzz");
+	}
+} 
+class Pig extends Animal
+{
+	public void animalSound()
+	{
+		System.out.println("The pig says: wee wee");
+	}
+}
+class Main2
+{
+	public static void main(String args[])
+	{
+		Pig mypig = new Pig();
+		mypig.animalSound();
+		mypig.sleep();
+	}
+}
